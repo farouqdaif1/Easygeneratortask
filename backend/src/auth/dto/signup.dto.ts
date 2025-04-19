2d333d449cddc72bf6c629f842cc9ab926805422
farouqdaif1 <daiffarouq@gmail.com>
2025-04-19 22:35:53 +0200

    Add DTOs

---
+++ backend/src/auth/dto/signup.dto.ts
@@ -0,0 +1,17 @@
+import { IsEmail, IsString, MinLength, Matches } from 'class-validator';
+
+export class SignupDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(3)
+  name: string;
+
+  @IsString()
+  @MinLength(8)
+  @Matches(/[a-zA-Z]/)
+  @Matches(/\d/)
+  @Matches(/[!@#$%^&*(),.?":{}|<>]/)
+  password: string;
+}
